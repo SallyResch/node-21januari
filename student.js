983be6ba60df787285ec50ecb85fc125e506492b
SallyResch <sallyresch27@gmail.com>
2026-01-22 15:32:26 +0100

    added queries into the node.js mix and the files that I fetch

--- student.js
+++ student.js
@@ -8,7 +8,7 @@ const navigation = () => {
     <a href="/">Home</a>
     <a href="/student">Student</a>
     <a href="/grades">Grades</a>
-    </nav>`)
+    </nav`)
 }
 
 const header = (headerTitle) => {
@@ -24,42 +24,57 @@ const footer = (footerTitle) => {
 http.createServer((req, res) => {
   let address = url.parse(req.url, true);
   const currentPath = address.pathname;
+  const searchTerm = address.query;
   res.writeHead(200, "All good, Status 200", { "Content-type": "text/html" })
 
   if (currentPath === "/") {
-    res.write(navigation());
-    res.write(header("Bestest school"));
-    res.write(footer("homeFooter"));
-    res.end();
+    fs.readFile("./content/navigation.html", (err, data) => {
+      if (err) {
+        res.write("Something went wrong")
+      } else {
+        res.write(data);
+      }
+      res.write(header("Bestest school"));
+      res.write(footer("homeFooter"));
+      res.end();
+    })
   }
+
   if (currentPath === "/student") {
     res.write(navigation());
     res.write(header("Hello Student!"));
-    fs.readFile("./content/studentDescription.txt", (err, data) => {
+    fs.readFile('./content/student.html', (err, data) => {
       if (err) {
-        res.write("Something went wrong");
-        res.end();
+        res.write("Its broken ffs!");
       } else {
-        res.write(data)
+        res.write(data);
       }
-      res.write(footer("studentFooter"));
-      res.end()
-    });
+      res.write(footer("StudentFooter"));
+      res.end();
+    })
+
   }
 
   if (currentPath === "/grades") {
     res.write(navigation());
     res.write(header("Your grades:"));
-    fs.readFile("./content/grades.html", (err, data) => {
-      if (err) {
-        res.write("ERROR");
-        return
-      } else {
-        res.write(data);
-      }
-      res.write(footer("gradesFooter"));
-      res.end()
-    });
 
+    if (searchTerm.grade === "allgrades") {
+      fs.readFile("./content/grades.html", (err, data) => {
+        if (err) {
+          res.write("ERROR");
+          res.end()
+          return
+        }
+        res.write(data);
+        res.write(footer("gradesFooter"));
+        res.end();
+      });
+    } else {
+      res.write("No grades to show");
+      res.write(`<a href="/grades?grade=allgrades">All grades</a>`)
+      res.write(footer("gradesFooter"));
+      res.end();
+    }
   }
 }).listen(1337, () => console.log("Webserver connected to port 1337"));
